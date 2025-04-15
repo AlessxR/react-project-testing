77ea71b805168300788301b4cd6d7d6a8b493d60
Alexxoooo <alexandaliev33@gmail.com>
2025-04-15 14:35:01 +0300

    Базовая верстка

---
+++ src/components/app/app.js
@@ -0,0 +1,28 @@
+import AppFilter from '../app-filter/app-filter';
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import EmployeesList from '../employees-list/employees-list';
+
+
+import './app.css';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+function App() {
+    return (
+        <div className="app">
+
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+            </div>            
+        
+            <EmployeesList />
+            <EmployeesAddForm />
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <div>
+            <input className="form-control search-input" type="text" placeholder="Найти сотрудника" />
+
+        </div>
+    );
+}; 
+
+export default SearchPanel;
